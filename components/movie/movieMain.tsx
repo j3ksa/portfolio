8345c7721ec7b01ee20705da91fd6314621c5e31
j3ksa <lukasz.jeksa@gmail.com>
2023-06-29 17:20:17 +0200

    C-6 Added safecases

--- components/movie/movieMain.tsx
+++ components/movie/movieMain.tsx
@@ -2,108 +2,216 @@ import React, { useRef, useState } from "react"
 import Image from "next/image"
 import { debounce } from "utils"
 import { fetchMovies, getMoreInfo } from "pages/api"
-import { InitialMovie } from "interfaces/movie"
+import { InitialMovie, MovieDetailed } from "interfaces/movie"
+import { AiOutlineClose } from "react-icons/ai"
 
 const MovieMain = () => {
     const leftInput = useRef()
     const rightInput = useRef()
 
-    const [leftLoaded, setLeftLoaded] = useState(false)
-    const [leftList, setLeftList] = useState<InitialMovie[]>()
+    const [leftList, setLeftList] = useState<InitialMovie[]>([])
+    const [rightList, setRightList] = useState<InitialMovie[]>([])
 
-    const [rightLoaded, setRightLoaded] = useState(false)
-    const [rightList, setLRightList] = useState<InitialMovie[]>()
+    const [leftPicked, setLeftPicked] = useState<MovieDetailed>()
+    const [rightPicked, setRightPicked] = useState<MovieDetailed>()
+
+    const [leftError, setLeftError] = useState('')
+    const [rightError, setRightError] = useState('')
 
     const leftSummary = async (e: React.ChangeEvent<HTMLInputElement>) => {
-        setLeftLoaded(false)
-        const results = await fetchMovies(e.target.value)
-        setLeftList(results.Search)
-        setLeftLoaded(true)
+        try {
+            const results = await fetchMovies(e.target.value)
+            if(results.Response === "False") {
+                setLeftList([])
+                setLeftError(results.Error)
+            } else {
+                setLeftError('')
+                setLeftList(results.Search)
+            }
+        }
+        catch(error) {
+            console.log(error)
+        }
     }
 
     const rightSummary = async(e: React.ChangeEvent<HTMLInputElement>) => {
-        setRightLoaded(false)
-        const results = await fetchMovies(e.target.value)
-        setLRightList(results.Search)
-        setRightLoaded(true)
+        try {
+            const results = await fetchMovies(e.target.value)
+            if(results.Response === "False") {
+                setLeftList([])
+                setRightError(results.Error)
+            } else {
+                setRightError('')
+                setRightList(results.Search)
+            }
+        }
+        catch(error) {
+            console.log(error)
+        }
     }
 
-    const handleClick = () => {
-        
+    const handleLeftClick = async (imdbID: string) => {
+        setLeftList([])
+        const results = await getMoreInfo(imdbID)
+        setLeftPicked(results)
+    }
+
+    const handleRightClick = async (imdbID: string) => {
+        setRightList([])
+        const results = await getMoreInfo(imdbID)
+        setRightPicked(results)
     }
 
     return (
-        <div className="container bg-white">
+        <div className="container bg-white py-4">
             <div className="grid grid-cols-2">
-                <div className="flex flex-col items-center">
-                    <p>First movie</p>
-                    <input 
-                        id="leftInput"
-                        ref={leftInput}
-                        type="text"
-                        className="outline-none border border-blue-500 placeholder:px-2 px-2"
-                        placeholder="min. 3 letters"
-                        onChange={debounce((e: React.ChangeEvent<HTMLInputElement>) => leftSummary(e))}
-                    />
-                    {leftLoaded && 
-                        <div className="">
-                            <div className="grid grid-cols-3 text-xl">
-                                <p className="place-self-stretch flex items-center justify-center">Poster:</p>
-                                <p>Title:</p>
-                                <p className="place-self-stretch flex items-center justify-center">Year:</p>
+                <div className="flex flex-col items-center relative">
+                    <p className="text-xl font-bold mb-2">First movie</p>
+                    {leftPicked ?
+                        <div className="flex items-center space-x-2 relative">
+                            <Image
+                                src={leftPicked.Poster === 'N/A' ? '/assets/notAvailable.png' : leftPicked.Poster}
+                                alt="moviePoster"
+                                width={0}
+                                height={0}
+                                sizes="300px"
+                                quality={100}
+                                className="w-[150px] object-cover aspect-[2/3] place-self-stretch mx-auto"
+                            />
+                            <div className="flex flex-col justify-center items-start">
+                                <p>{leftPicked.Title}</p>
+                                <p>{leftPicked.BoxOffice}</p>
+                                <p>{leftPicked.Actors}</p>
+                                <p>{leftPicked.Awards}</p>
+                                <p>{leftPicked.Director}</p>
+                                <p>{leftPicked.Genre}</p>
+                                <p>{leftPicked.Released}</p>
+                                <p>{leftPicked.Runtime}</p>
+                                <p>{leftPicked.Year}</p>
+                                {leftPicked.Ratings.map(rating => {
+                                    return(
+                                        <div className="flex">
+                                            <p>{rating.Source} <span className="mx-1">-</span></p>
+                                            <p> {rating.Value}</p>
+                                        </div>
+                                    )
+                                })}
                             </div>
-                            {leftList?.map((movie, index) => {
-                                return (
-                                    <div key={index} className="grid grid-cols-3 mb-2">
-                                        <Image
-                                            src={movie.Poster}
-                                            alt="moviePoster"
-                                            width={50}
-                                            height={50}
-                                            quality={100}
-                                            className="object-cover aspect-[2/3] place-self-stretch mx-auto"
-                                        />
-                                        <p className="place-self-stretch flex items-center justify-start">{movie.Title}</p>
-                                        <p className="place-self-stretch flex items-center justify-center">{movie.Year}</p>
+                            <AiOutlineClose className="absolute top-0 right-0 cursor-pointer" onClick={setLeftPicked.bind(this, null)}/>
+                        </div> :
+                        <div className="absolute top-8 left-0 bg-white flex flex-col items-center">
+                            <input 
+                                id="leftInput"
+                                ref={leftInput}
+                                type="text"
+                                className="outline-none border border-blue-500 placeholder:px-2 px-2 mx-auto"
+                                placeholder="min. 3 letters"
+                                onChange={debounce((e: React.ChangeEvent<HTMLInputElement>) => leftSummary(e))}
+                            />
+                            {leftList.length > 0 && 
+                                <div className="">
+                                    <div className="grid grid-cols-3 text-xl">
+                                        <p className="place-self-stretch flex items-center justify-center">Poster:</p>
+                                        <p>Title:</p>
+                                        <p className="place-self-stretch flex items-center justify-center">Year:</p>
                                     </div>
-                                )
-                            })}
+                                    {leftList?.map((movie, index) => {
+                                        return (
+                                            <div key={index} className="grid grid-cols-3 mb-2" onClick={() => handleLeftClick(movie.imdbID)}>
+                                                <Image
+                                                    src={movie.Poster === 'N/A' ? '/assets/notAvailable.png' : movie.Poster}
+                                                    alt="moviePoster"
+                                                    width={50}
+                                                    height={50}
+                                                    quality={100}
+                                                    sizes="75px"
+                                                    className="object-cover aspect-[2/3] place-self-stretch mx-auto"
+                                                />
+                                                <p className="place-self-stretch flex items-center justify-start">{movie.Title}</p>
+                                                <p className="place-self-stretch flex items-center justify-center">{movie.Year}</p>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            }
+                            {leftError && <p className="text-red-500">{leftError}</p>}
                         </div>
                     }
                 </div>
-                <div className="flex flex-col items-center">
-                    <p>Second movie</p>
-                    <input 
-                        id="leftInput"
-                        ref={rightInput}
-                        type="text"
-                        placeholder="min. 3 letters"
-                        className="outline-none border border-blue-500 placeholder:px-2 px-2"
-                        onChange={debounce((e: React.ChangeEvent<HTMLInputElement>) => rightSummary(e))}
-                    />
-                    {rightLoaded && 
-                        <div className="">
-                            <div className="grid grid-cols-3 text-xl">
-                                <p className="place-self-stretch flex items-center justify-center">Poster:</p>
-                                <p>Title:</p>
-                                <p className="place-self-stretch flex items-center justify-center">Year:</p>
+                <div className="flex flex-col items-center relative">
+                    <p className="text-xl font-bold mb-2">Second movie</p>
+                    {rightPicked ?
+                        <div className="flex items-center space-x-2 relative">
+                            <Image
+                                src={rightPicked.Poster === 'N/A' ? '/assets/notAvailable.png' : rightPicked.Poster }
+                                alt="moviePoster"
+                                width={0}
+                                height={0}
+                                sizes="300px"
+                                quality={100}
+                                className="w-[150px] object-cover aspect-[2/3] place-self-stretch mx-auto"
+                            />
+                            <div className="flex flex-col justify-center items-start">
+                                <p>{rightPicked.Title}</p>
+                                <p>{rightPicked.BoxOffice}</p>
+                                <p>{rightPicked.Actors}</p>
+                                <p>{rightPicked.Awards}</p>
+                                <p>{rightPicked.Director}</p>
+                                <p>{rightPicked.Genre}</p>
+                                <p>{rightPicked.Released}</p>
+                                <p>{rightPicked.Runtime}</p>
+                                <p>{rightPicked.Year}</p>
+                                {rightPicked.Ratings.map(rating => {
+                                    return(
+                                        <div className="flex">
+                                            <p>{rating.Source} <span className="mx-1">-</span></p>
+                                            
+                                            <p> {rating.Value}</p>
+                                        </div>
+                                    )
+                                })}
                             </div>
-                            {rightList.map((movie, index) => {
-                                return (
-                                    <div key={index} className="grid grid-cols-3 mb-2">
-                                        <Image
-                                            src={movie.Poster}
-                                            alt="moviePoster"
-                                            width={50}
-                                            height={50}
-                                            quality={100}
-                                            className="object-cover aspect-[2/3] place-self-stretch mx-auto"
-                                        />
-                                        <p className="place-self-stretch flex items-center justify-start">{movie.Title}</p>
-                                        <p className="place-self-stretch flex items-center justify-center">{movie.Year}</p>
+                            <AiOutlineClose className="absolute top-0 right-0 cursor-pointer" onClick={setRightPicked.bind(this, null)}/>
+                        </div> :
+                        <div className="absolute top-8 right-0 bg-white flex flex-col items-center">
+                            <input 
+                                id="leftInput"
+                                ref={rightInput}
+                                type="text"
+                                placeholder="min. 3 letters"
+                                className="outline-none border border-blue-500 placeholder:px-2 px-2"
+                                onChange={debounce((e: React.ChangeEvent<HTMLInputElement>) => rightSummary(e))}
+                            />
+                            {rightList.length > 0 && 
+                                <div className="">
+                                    <div className="grid grid-cols-3 text-xl">
+                                        <p className="place-self-stretch flex items-center justify-center">Poster:</p>
+                                        <p>Title:</p>
+                                        <p className="place-self-stretch flex items-center justify-center">Year:</p>
                                     </div>
-                                )
-                            })}
+                                    {rightList.map((movie, index) => {
+                                        return (
+                                            <div 
+                                                key={index} 
+                                                className="grid grid-cols-3 mb-2"
+                                                onClick={() => handleRightClick(movie.imdbID)}
+                                            >
+                                                <Image
+                                                    src={movie.Poster === 'N/A' ? '/assets/notAvailable.png' : movie.Poster}
+                                                    alt="moviePoster"
+                                                    width={50}
+                                                    height={50}
+                                                    quality={100}
+                                                    className="object-cover aspect-[2/3] place-self-stretch mx-auto"
+                                                />
+                                                <p className="place-self-stretch flex items-center justify-start">{movie.Title}</p>
+                                                <p className="place-self-stretch flex items-center justify-center">{movie.Year}</p>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            }
+                            {rightError && <p className="text-red-500">{rightError}</p>}
                         </div>
                     }
                 </div>
--- interfaces/movie.ts
+++ interfaces/movie.ts
@@ -5,3 +5,36 @@ export interface InitialMovie {
     Year: string
     imdbID: string
 }
+
+interface Ratings {
+    Source: string
+    Value: string
+}
+
+export interface MovieDetailed {
+    Actors: string
+    Awards: string
+    BoxOffice: string
+    Country: string
+    DVD: string
+    Director: string
+    Genre: string
+    Language: string
+    Metascore: string
+    Plot: string
+    Poster: string
+    Production: string
+    Rated: string
+    Ratings: Ratings[]
+    Released: string
+    Response: string
+    Runtime: string
+    Title: string
+    Type: string
+    Website: string
+    Writer: string
+    Year: string
+    imdbID: string
+    imdbRating: string
+    imdbVotes: string
+}
